179fa515c75d687706afc9fefb4b962a637059d5
sergey <sergey>
2017-02-07 17:41:19 +0300

    clear

---
+++ src/TwitterBundle/Command/FetchTweetsCommand.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace TwitterBundle\Command;
+
+use Endroid\Twitter\Twitter;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use TwitterBundle\Entity\Hashtag;
+use TwitterBundle\Entity\Tweet;
+use TwitterBundle\Repository\HashtagRepository;
+use TwitterBundle\Repository\TweetRepository;
+
+/**
+ * Class FetchTweetsCommand
+ *
+ * @package TwitterBundle\Command
+ */
+class FetchTweetsCommand extends ContainerAwareCommand
+{
+    /**
+     *
+     */
+    public function configure()
+    {
+        $this
+            ->setName('twitter:fetch-tweets')
+            ->setDescription('Fetch latest tweets to DB. To update tweets use another command');
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $container = $this->getContainer();
+
+        /** @var Twitter $api */
+        $api = $container->get('endroid.twitter');
+        $apiResponse = $api->query('statuses/user_timeline', 'GET', 'json', ['screen_name' => 'qslipper_ru']); // bbcrussian
+        $apiTweets = json_decode($apiResponse->getContent());
+
+        $doctrine = $container->get('doctrine');
+
+        $em = $doctrine->getManager();
+
+        /** @var TweetRepository $rTweet */
+        $rTweet = $doctrine->getRepository('TwitterBundle:Tweet');
+        /** @var HashtagRepository $rHashtag */
+        $rHashtag = $doctrine->getRepository('TwitterBundle:Hashtag');
+
+        foreach ($apiTweets as $apiTweet) {
+            $tweet = $rTweet->findOneBy(['id_str' => $apiTweet->id_str]);
+            $tweet = $tweet ? $tweet : new Tweet();
+
+            $tweet
+                ->setIdStr($apiTweet->id_str)
+                ->setText($apiTweet->text)
+                ->setCreatedAt(strtotime($apiTweet->created_at))
+                ->setFavorited($apiTweet->favorited ? $apiTweet->favorited : 0)
+                ->setRetweetCount($apiTweet->retweet_count ? $apiTweet->retweet_count : 0);
+
+            $em->persist($tweet);
+            $em->flush();
+
+            $output->writeln('Tweet #' . $tweet->getId() . ' was updated.');
+
+            $rHashtag->deleteByTweetID($tweet->getId());
+
+            // insert new\updated if exists
+            if (isset($apiTweet->entities->hashtags) && !empty($apiTweet->entities->hashtags)) {
+                foreach ($apiTweet->entities->hashtags as $apiHashtag) {
+                    $hashtag = new Hashtag();
+                    $hashtag->setHashtag($apiHashtag->text)->setTweet($tweet);
+
+                    $em->persist($hashtag);
+                    $em->flush();
+
+                    $output->writeln('Hashtags for tweet #' . $tweet->getId() . ' was updated.');
+                }
+            }
+        }
+    }
+}
--- src/TwitterBundle/Controller/DefaultController.php
+++ src/TwitterBundle/Controller/DefaultController.php
@@ -2,11 +2,7 @@
 
 namespace TwitterBundle\Controller;
 
-use Endroid\Twitter\Twitter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use TwitterBundle\Entity\Hashtag;
-use TwitterBundle\Entity\Tweet;
-use TwitterBundle\Repository\HashtagRepository;
 use TwitterBundle\Repository\TweetRepository;
 
 /**
@@ -24,58 +20,21 @@ class DefaultController extends Controller
      */
     public function indexAction($page, $hashtag = null)
     {
-        /** @var Twitter $api */
-        $api = $this->get('endroid.twitter');
-        $apiResponse = $api->query('statuses/user_timeline', 'GET', 'json', ['screen_name' => 'qslipper_ru']); // bbcrussian
-        $apiTweets = json_decode($apiResponse->getContent());
-
-        $em = $this->getDoctrine()->getManager();
-
         /** @var TweetRepository $rTweet */
         $rTweet = $this->getDoctrine()->getRepository('TwitterBundle:Tweet');
-        /** @var HashtagRepository $rHashtag */
-        $rHashtag = $this->getDoctrine()->getRepository('TwitterBundle:Hashtag');
 
-        foreach ($apiTweets as $apiTweet) {
-            $tweet = $rTweet->findOneBy(['id_str' => $apiTweet->id_str]);
-            $tweet = $tweet ? $tweet : new Tweet();
-
-            $tweet
-                ->setIdStr($apiTweet->id_str)
-                ->setText($apiTweet->text)
-                ->setCreatedAt(strtotime($apiTweet->created_at))
-                ->setFavorited($apiTweet->favorited ? $apiTweet->favorited : 0)
-                ->setRetweetCount($apiTweet->retweet_count ? $apiTweet->retweet_count : 0);
-
-            $em->persist($tweet);
-            $em->flush();
-
-            $rHashtag->deleteByTweetID($tweet->getId());
-
-            // insert new\updated if exists
-            if (isset($apiTweet->entities->hashtags) && !empty($apiTweet->entities->hashtags)) {
-                foreach ($apiTweet->entities->hashtags as $apiHashtag) {
-                    $hashtag = new Hashtag();
-                    $hashtag->setHashtag($apiHashtag->text)->setTweet($tweet);
-
-                    $em->persist($hashtag);
-                    $em->flush();
-                }
-            }
-        }
-
-        $limit = 5;
+        $limit = 10;
         $tweetsPaginator = $rTweet->getPaginated($page, $limit);
-        $maxPages = ceil($tweetsPaginator->count() / $limit);
-        $thisPage = $page;
+        $maxPage = ceil($tweetsPaginator->count() / $limit);
+        $curPage = $page;
 
         $topRetweeted = $rTweet->getTopRetweeted();
 
         return $this->render('TwitterBundle:Default:index.html.twig', [
             'tweets' => $tweetsPaginator,
             'topRetweeted' => $topRetweeted,
-            'maxPages' => $maxPages,
-            'thisPage' => $thisPage
+            'maxPage' => $maxPage,
+            'curPage' => $curPage
         ]);
     }
 }
